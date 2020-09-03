47c6140586dab21a1bdaa716e6fc55a2c9c503d8
vpan720 <vpan720@gmail.com>
2020-09-03 16:36:02 +0530

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -10,7 +10,7 @@ export class ProductService {
   private _albumUrl = "../assets/album.json";
 
   getAlbum(id: Number) {
-    return this._http.get(this._albumUrl);
+    return this._http.get(this._albumUrl).map;
   }
 
 }
