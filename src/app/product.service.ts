e9d9a4009e258d0253a0a61beded2adbb71a1a75
vpan720 <vpan720@gmail.com>
2020-09-03 16:46:14 +0530

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -5,12 +5,12 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
 
-  constructor(private _http: Http) { }
-
   private _albumUrl = "../assets/album.json";
 
+  constructor(private _http: Http) { }
+
   getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map;
+    return this._http.get(this._albumUrl).map((response)=>response.json());
   }
 
 }
